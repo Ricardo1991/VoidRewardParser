a0b6fe16abd170226345781c1378b02480623579
Ricardo Ribeiro <ricardoribeiro_1991@hotmail.com>
2018-07-15 00:34:19 +0100

    black and white screen capture

--- VoidRewardParser/Logic/ScreenCapture.cs
+++ VoidRewardParser/Logic/ScreenCapture.cs
@@ -14,7 +14,6 @@ namespace VoidRewardParser.Logic
 {
     public class ScreenCapture
     {
-
         public static async Task<string> ParseTextAsync()
         {
             try
@@ -37,13 +36,48 @@ namespace VoidRewardParser.Logic
         {
             int width = (int)SystemParameters.FullPrimaryScreenWidth;
             int height = (int)SystemParameters.FullPrimaryScreenHeight;
-            using (var bitmap = new Bitmap(width, height, PixelFormat.Format24bppRgb))
+            Bitmap bitmap = MakeGrayscale3(new Bitmap(width, height, PixelFormat.Format24bppRgb));
             using (var graphics = Graphics.FromImage(bitmap))
             {
                 graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
                 graphics.Save();
                 bitmap.Save(stream, ImageFormat.Png);
             }
+        }
+
+        public static Bitmap MakeGrayscale3(Bitmap original)
+        {
+            //create a blank bitmap the same size as original
+            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+
+            //get a graphics object from the new image
+            Graphics g = Graphics.FromImage(newBitmap);
+
+            //create the grayscale ColorMatrix
+            ColorMatrix colorMatrix = new ColorMatrix(
+               new float[][]
+               {
+                    new float[] {.3f, .3f, .3f, 0, 0},
+                    new float[] {.59f, .59f, .59f, 0, 0},
+                    new float[] {.11f, .11f, .11f, 0, 0},
+                    new float[] {0, 0, 0, 1, 0},
+                    new float[] {0, 0, 0, 0, 1}
+               });
+
+            //create some image attributes
+            ImageAttributes attributes = new ImageAttributes();
+
+            //set the color matrix attribute
+            attributes.SetColorMatrix(colorMatrix);
+
+            //draw the original image on the new image
+            //using the grayscale color matrix
+            g.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height),
+               0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
+
+            //dispose the Graphics object
+            g.Dispose();
+            return newBitmap;
         }
 
         private static async Task<string> RunOcr(IRandomAccessStream stream)
